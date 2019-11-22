5c5fb749e02faa954f64bd973bdbcc190223c033
MohammadShameem <mohammadshamimcse@gmail.com>
2019-11-22 22:40:17 +0600

    first commit

---
+++ segment_main.py
@@ -0,0 +1,174 @@
+from math import sqrt, atan2, pi
+import numpy as np
+from PIL import Image, ImageDraw
+from math import sqrt, pi, cos, sin
+from collections import defaultdict
+import sys
+
+
+def canny_edge_detector(input_image):
+    input_pixels = input_image.load()
+    width = input_image.width
+    height = input_image.height
+
+    # Transform the image to grayscale
+    grayscaled = compute_grayscale(input_pixels, width, height)
+
+    # Blur it to remove noise
+    blurred = compute_blur(grayscaled, width, height)
+
+    # Compute the gradient
+    gradient, direction = compute_gradient(blurred, width, height)
+
+    # Non-maximum suppression
+    filter_out_non_maximum(gradient, direction, width, height)
+
+    # Filter out some edges
+    keep = filter_strong_edges(gradient, width, height, 20, 25)
+
+    return keep
+
+# The function Transform the image to grayscale
+def compute_grayscale(input_pixels, width, height):
+    grayscale = np.empty((width, height))
+    for x in range(width):
+        for y in range(height):
+            pixel = input_pixels[x, y]
+            grayscale[x, y] = (pixel[0] + pixel[1] + pixel[2]) / 3
+    return grayscale
+
+# The function Blur it to remove noise
+def compute_blur(input_pixels, width, height):
+    # Keep coordinate inside image
+    clip = lambda x, l, u: l if x < l else u if x > u else x
+
+    # Gaussian kernel
+    kernel = np.array([
+        [1 / 256,  4 / 256,  6 / 256,  4 / 256, 1 / 256],
+        [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
+        [6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256],
+        [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
+        [1 / 256,  4 / 256,  6 / 256,  4 / 256, 1 / 256]
+    ])
+
+    # Middle of the kernel
+    offset = len(kernel) // 2
+
+    # Compute the blurred image
+    blurred = np.empty((width, height))
+    for x in range(width):
+        for y in range(height):
+            acc = 0
+            for a in range(len(kernel)):
+                for b in range(len(kernel)):
+                    xn = clip(x + a - offset, 0, width - 1)
+                    yn = clip(y + b - offset, 0, height - 1)
+                    acc += input_pixels[xn, yn] * kernel[a, b]
+            blurred[x, y] = int(acc)
+    return blurred
+
+# The function  Compute the gradient
+def compute_gradient(input_pixels, width, height):
+    gradient = np.zeros((width, height))
+    direction = np.zeros((width, height))
+    for x in range(width):
+        for y in range(height):
+            if 0 < x < width - 1 and 0 < y < height - 1:
+                magx = input_pixels[x + 1, y] - input_pixels[x - 1, y]
+                magy = input_pixels[x, y + 1] - input_pixels[x, y - 1]
+                gradient[x, y] = sqrt(magx**2 + magy**2)
+                direction[x, y] = atan2(magy, magx)
+    return gradient, direction
+
+# The function Non-maximum suppression
+def filter_out_non_maximum(gradient, direction, width, height):
+    for x in range(1, width - 1):
+        for y in range(1, height - 1):
+            angle = direction[x, y] if direction[x, y] >= 0 else direction[x, y] + pi
+            rangle = round(angle / (pi / 4))
+            mag = gradient[x, y]
+            if ((rangle == 0 or rangle == 4) and (gradient[x - 1, y] > mag or gradient[x + 1, y] > mag)
+                    or (rangle == 1 and (gradient[x - 1, y - 1] > mag or gradient[x + 1, y + 1] > mag))
+                    or (rangle == 2 and (gradient[x, y - 1] > mag or gradient[x, y + 1] > mag))
+                    or (rangle == 3 and (gradient[x + 1, y - 1] > mag or gradient[x - 1, y + 1] > mag))):
+                gradient[x, y] = 0
+
+# The function Filter out some edges
+def filter_strong_edges(gradient, width, height, low, high):
+    # Keep strong edges
+    keep = set()
+    for x in range(width):
+        for y in range(height):
+            if gradient[x, y] > high:
+                keep.add((x, y))
+
+    # Keep weak edges next to a pixel to keep
+    lastiter = keep
+    while lastiter:
+        newkeep = set()
+        for x, y in lastiter:
+            for a, b in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+                if gradient[x + a, y + b] > low and (x+a, y+b) not in keep:
+                    newkeep.add((x+a, y+b))
+        keep.update(newkeep)
+        lastiter = newkeep
+
+    return list(keep)
+
+
+if __name__ == "__main__":
+    from PIL import Image, ImageDraw
+
+    my_image = Image.open(sys.argv[1])
+    input_image = my_image.resize((420, 300))
+    output_image = Image.new("RGB", input_image.size)
+
+
+    draw = ImageDraw.Draw(output_image)
+    for x, y in canny_edge_detector(input_image):
+        draw.point((x, y), (255, 255, 255))
+
+    output_image.show()
+
+    width, height = output_image.size
+    start = (int(0 * width), int(0 * height))
+    any_color = (255, 100, 150, 0)
+    ImageDraw.floodfill(output_image, xy=start, value=any_color)
+    output_image.show()
+
+    # Output image:
+    output_image = Image.new("RGB", input_image.size)
+    output_image.paste(input_image)
+    draw_result = ImageDraw.Draw(output_image)
+
+    # Find circles
+    rmin = int(sys.argv[2])
+    rmax = int(sys.argv[3])
+    steps = int(sys.argv[4])
+    threshold = float(sys.argv[5])
+
+    print("Min Radius = " + str(rmin) + ", Max Radius = " + str(rmax) + ", Steps = " + str(steps) + ", Threshold = " + str(threshold))
+    points = []
+    for r in range(rmin, rmax + 1):
+        for t in range(steps):
+            points.append((r, int(r * cos(2 * pi * t / steps)), int(r * sin(2 * pi * t / steps))))
+
+    acc = defaultdict(int)
+    for x, y in canny_edge_detector(input_image):
+        for r, dx, dy in points:
+            a = x - dx
+            b = y - dy
+            acc[(a, b, r)] += 1
+
+    circles = []
+    for k, v in sorted(acc.items(), key=lambda i: -i[1]):
+        x, y, r = k
+        if v / steps >= threshold and all((x - xc) ** 2 + (y - yc) ** 2 > rc ** 2 for xc, yc, rc in circles):
+            print(v / steps, x, y, r)
+            circles.append((x, y, r))
+
+    for x, y, r in circles:
+        draw_result.ellipse((x - r, y - r, x + r, y + r), outline=(255, 0, 0, 0), width=3)
+
+
+    output_image.show()
